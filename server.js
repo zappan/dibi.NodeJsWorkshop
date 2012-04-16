8eee0b61217616624f0aa57e6cecbfae82f17b37
Tomislav Capan <tomislav.capan@gmail.com>
2012-04-16 14:30:24 +0200

    Excercise #1: changing response by user agent; running with a debugger

--- server.js
+++ server.js
@@ -2,7 +2,22 @@ var http = require("http")
   , s = http.createServer();
 
 s.on("request", function(req, res) {
-  res.writeHead(200, {"Content-Type": "text/plain"});
-  res.end("Hello World\n");
+  var userAgentString = req.headers["user-agent"]
+    , responseHTML;
+
+  responseHTML = "<html>\n"
+      + "<head>\n"
+      + "<title>Node.js workshop</title>\n"
+      + "</head>\n"
+      + "<body>\n"
+      + "<h3>I'm learning Node</h3>\n"
+      + "<p>Page visited using: " + userAgentString + "</p>\n"
+      + "</body>\n"
+      + "</html>\n"
+    ;
+
+  res.writeHead(200, {"Content-Type": "text/html"});
+  res.end(responseHTML);
 }).listen(8081, "127.0.0.1");
+
 console.log("Server running at http://127.0.0.1:8081/");

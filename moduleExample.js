f4efdec7b9d3ebc4d1ec8effe6bcccee1749ac35
Tomislav Capan <tomislav.capan@gmail.com>
2012-04-16 16:29:41 +0200

    Excercise #4: Modules example

---
+++ moduleExample.js
@@ -0,0 +1,9 @@
+var fish = require('./lib/fish');
+
+console.log("Here's a fish called " + fish.name);
+fish.swim();
+fish.mouthbreath();
+fish.swim();
+fish.swim();
+fish.mouthbreath();
+fish.floparound();
